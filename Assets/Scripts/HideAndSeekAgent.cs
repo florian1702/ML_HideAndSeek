d3f7d6bd6796d66dbc324de74e052f15b5c01424
florian1702 <flo-woe@web.de>
2025-02-18 11:23:31 +0100

    PPO Config

--- Assets/Scripts/HideAndSeekAgent.cs
+++ Assets/Scripts/HideAndSeekAgent.cs
@@ -3,7 +3,6 @@ using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 
 public class HideAndSeekAgent : Agent
 {
@@ -47,7 +46,7 @@ public class HideAndSeekAgent : Agent
         {
             if (AgentSeesEntity(entity.gameObject, out RaycastHit hit))
             {
-                float[] obs = new float[10];
+                float[] obs = new float[7];
                 Vector3 relativePosition = entity.transform.position - transform.position;
                 obs[0] = relativePosition.x;
                 obs[1] = relativePosition.y;
@@ -57,10 +56,10 @@ public class HideAndSeekAgent : Agent
                 obs[5] = entity.GetComponent<Rigidbody>()?.linearVelocity.y ?? 0f;
                 obs[6] = entity.GetComponent<Rigidbody>()?.linearVelocity.z ?? 0f;
                 
-                Vector3 entityScale = entity.transform.localScale;
-                obs[7] = entityScale.x;
-                obs[8] = entityScale.y;
-                obs[9] = entityScale.z;
+                //Vector3 entityScale = entity.transform.localScale;
+                //obs[7] = entityScale.x;
+                //obs[8] = entityScale.y;
+                //obs[9] = entityScale.z;
                 sensor.AppendObservation(obs);
             }
         }
